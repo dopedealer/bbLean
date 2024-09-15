f302ddd05f8da18efadc8fb4ec18ac75bd1168a5
kam1 <xmalkut@gmail.com>
2024-09-16 00:09:47 +0300

    - fixed version display in blackbox.exe info - removed old inc file

--- blackbox/bbversion.h
+++ blackbox/bbversion.h
@@ -1,3 +1,13 @@
-#define BBLEAN_VERSION "1.17.1"
-#define BBLEAN_RELDATE "2009-12-08"
-#define BBLEAN_NUMVERSION 0,1,17,1
+
+
+#ifndef BBLEAN_VERSION
+#   define BBLEAN_VERSION "1.17.1"
+#endif
+
+#ifndef BBLEAN_RELDATE
+#   define BBLEAN_RELDATE "2009-12-08"
+#endif
+
+#ifndef BBLEAN_NUMVERSION
+#   define BBLEAN_NUMVERSION 0,1,17,1
+#endif
